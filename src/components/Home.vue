fc628dea462ef5823f7de4b343bd3f456f828b34
tom110 <254449149@qq.com>
2020-02-17 16:37:05 +0800

    修改ui

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,7 +3,7 @@
     <!-- 头部区域 -->
     <el-header>
       <div>
-        <img src="../assets/heima.png" alt="">
+        <img src="../assets/heima.png" alt />
         <span>地质大数据平台</span>
       </div>
       <el-button type="info" @click="logout">退出</el-button>
@@ -14,9 +14,18 @@
       <el-aside :width="isCollapse ? '64px' : '200px'">
         <div class="toggle-button" @click="toggleCollapse">|||</div>
         <!-- 侧边栏菜单区域 -->
-        <el-menu background-color="#333744" text-color="#fff" active-text-color="#409EFF" unique-opened :collapse="isCollapse" :collapse-transition="false" router :default-active="activePath">
+        <el-menu
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#409EFF"
+          unique-opened
+          :collapse="isCollapse"
+          :collapse-transition="false"
+          router
+          :default-active="activePath"
+        >
           <!-- 一级菜单 -->
-          <el-submenu>
+          <el-submenu index="0">
             <!-- 一级菜单的模板区域 -->
             <template slot="title">
               <!-- 图标 -->
@@ -26,7 +35,7 @@
             </template>
 
             <!-- 二级菜单 -->
-            <el-menu-item @click="saveNavState('/welcome')">
+            <el-menu-item index="/welcome" @click="saveNavState('/welcome')">
               <template slot="title">
                 <!-- 图标 -->
                 <i class="el-icon-menu"></i>
@@ -46,7 +55,12 @@
             </template>
 
             <!-- 二级菜单 -->
-            <el-menu-item :index="'/' + subItem.path" v-for="subItem in item.children" :key="subItem.id" @click="saveNavState('/' + subItem.path)">
+            <el-menu-item
+              :index="'/' + subItem.path"
+              v-for="subItem in item.children"
+              :key="subItem.id"
+              @click="saveNavState('/' + subItem.path)"
+            >
               <template slot="title">
                 <!-- 图标 -->
                 <i class="el-icon-menu"></i>
@@ -59,8 +73,13 @@
       </el-aside>
       <!-- 右侧内容主体 -->
       <el-main>
-        <!-- 路由占位符 -->
-        <router-view></router-view>
+        <div class="earth">
+          <div ref="earthContainer" class="earthContainer"></div>
+        </div>
+        <div ref="routerView" class="routerView" v-bind:class="{displayNone:isDisplay}">
+          <!-- 路由占位符 -->
+          <router-view></router-view>
+        </div>
       </el-main>
     </el-container>
   </el-container>
@@ -68,8 +87,12 @@
 
 <script>
 export default {
-  data () {
+  data() {
     return {
+      // eslint-disable-next-line vue/no-reserved-keys
+      _earth: undefined, // 注意：Earth和Cesium的相关变量放在vue中，必须使用下划线作为前缀！
+      // eslint-disable-next-line vue/no-reserved-keys
+      _bgImagery: undefined,
       // 左侧菜单数据
       menulist: [],
       iconsObj: {
@@ -82,31 +105,73 @@ export default {
       // 是否折叠
       isCollapse: false,
       // 被激活的链接地址
-      activePath: ''
+      activePath: '',
+      isDisplay: true
     }
   },
-  created () {
+  created() {
     this.getMenuList()
     this.activePath = window.sessionStorage.getItem('activePath')
   },
+  // 1.1 资源创建
+  mounted() {
+    // 1.1.1 创建地球
+    // eslint-disable-next-line no-undef
+    var earth = new XE.Earth(this.$refs.earthContainer)
+
+    // 1.1.2 添加默认地球影像
+    earth.sceneTree.root = {
+      children: [
+        {
+          czmObject: {
+            name: '默认离线影像',
+            xbsjType: 'Imagery',
+            xbsjImageryProvider: {
+              createTileMapServiceImageryProvider: {
+                // eslint-disable-next-line no-undef
+                url: XE.HTML.cesiumDir + 'Assets/Textures/NaturalEarthII',
+                fileExtension: 'jpg'
+              },
+              type: 'createTileMapServiceImageryProvider'
+            }
+          }
+        }
+      ]
+    }
+
+    this._earth = earth
+
+    // 仅为调试方便用
+    window.earth = earth
+  },
+  // 1.2 资源销毁
+  beforeDestroy() {
+    // vue程序销毁时，需要清理相关资源
+    this._earth = this._earth && this._earth.destroy()
+  },
   methods: {
-    logout () {
+    logout() {
       window.sessionStorage.clear()
       this.$router.push('/login')
     },
     // 获取所有的菜单
-    async getMenuList () {
+    async getMenuList() {
       const { data: res } = await this.$http.get('menus')
       if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
       this.menulist = res.data
       console.log(res)
     },
     // 点击按钮，切换菜单的折叠与展开
-    toggleCollapse () {
+    toggleCollapse() {
       this.isCollapse = !this.isCollapse
     },
     // 保存链接的激活状态
-    saveNavState (activePath) {
+    saveNavState(activePath) {
+      if (activePath === '/welcome') {
+        this.isDisplay = true
+      } else {
+        this.isDisplay = false
+      }
       window.sessionStorage.setItem('activePath', activePath)
       this.activePath = activePath
     }
@@ -144,6 +209,7 @@ export default {
 
 .el-main {
   background-color: #eaedf1;
+  padding: 0;
 }
 
 .iconfont {
@@ -159,4 +225,25 @@ export default {
   letter-spacing: 0.2em;
   cursor: pointer;
 }
+.earth {
+  height: 100%;
+  width: 100%;
+  position: fixed;
+  background: red;
+}
+.routerView {
+  padding: 20px;
+  height: 100%;
+  width: 82%;
+  position: fixed;
+  overflow-y: auto;
+}
+.earthContainer {
+  height: 100%;
+  width: 100%;
+}
+.displayNone {
+  display: none;
+}
+
 </style>
